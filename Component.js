3323fb54aaf71fcd25813ccf4bea0922e0cd24fb
j1wu <ji.wu@outlook.com>
2015-02-18 06:52:45 +0800

    refactor base on ui5 application best practice guideline, started with Component.js, finished initial refactoring

--- Component.js
+++ Component.js
@@ -7,36 +7,44 @@ sap.ui.core.UIComponent.extend("sap.usrmgm.Component", {
     dependencies: {
       libs: ["sap.m", "sap.ui.layout"],
     },
+    rootView: "sap.usrmgm.view.App",
     config: {
+      resourceBundle: "i18n/messageBundle.properties",
       serviceConfig: {
         name: "AG3",
         serviceUrl: "https://ldai1ag3.wdf.sap.corp:44355/sap/opu/odata/sap/ZWJ_USERS_ODATA_SRV/"
       }
-    }
+    },
+    routing: {
+      // to be implemented
+    },
   },
 
-  createContent: function() {
+  init: function() {
+    sap.ui.core.UIComponent.prototype.init.apply(this, arguments);
+
     var mConfig = this.getMetadata().getConfig();
+    var rootPath = jQuery.sap.getModulePath("sap.usrmgm");
+
+    var i18nModel = new sap.ui.model.resource.ResourceModel({
+      bundleUrl: [rootPath, mConfig.resourceBundle].join('/')
+    });
+
     var sServiceUrl = mConfig.serviceConfig.serviceUrl;
     var oModel = new sap.ui.model.odata.ODataModel(sServiceUrl, true);
     this.setModel(oModel);
-    var oApp = new sap.m.SplitApp({
-      id: "SplitApp"
+
+    var deviceModel = new sap.ui.model.json.JSONModel({
+            isTouch : sap.ui.Device.support.touch,
+            isNoTouch : !sap.ui.Device.support.touch,
+            isPhone : sap.ui.Device.system.phone,
+            isNoPhone : !sap.ui.Device.system.phone,
+            listMode : sap.ui.Device.system.phone ? "None" : "SingleSelectMaster",
+            listItemType : sap.ui.Device.system.phone ? "Active" : "Inactive"
     });
-    var oMaster = sap.ui.view({
-      id: "MasterView",
-      viewName: "sap.usrmgm.view.Master",
-      type: sap.ui.core.mvc.ViewType.XML
-    });
-    var oDetail = sap.ui.view({
-      id: "DetailView",
-      viewName: "sap.usrmgm.view.Detail",
-      type: sap.ui.core.mvc.ViewType.XML
-    });
-    oApp.addMasterPage(oMaster);
-    oApp.addDetailPage(oDetail);
-    oApp.setInitialMaster("MasterView");
-    oApp.setInitialDetail("DetailView");
-    return oApp;
+    deviceModel.setDefaultBindingMode("OneWay");
+    this.setModel(deviceModel, "device");
+
+    this.getRouter().initialize();
   }
 });
